4449012cea65ea45d3c1209c28cc4ae8f1eeffdc
Robert Kaye <rob@eorbit.net>
2011-07-11 00:22:30 -0700

    Documented correct connections. Added support for the wheel rotation sensor

--- atmel/poibot.c
+++ atmel/poibot.c
@@ -1,9 +1,9 @@
 #include <avr/io.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
-//#define F_CPU 16000000UL
 #include <util/delay.h>
 
 #include "debug.h"
@@ -13,17 +13,26 @@
 #define cbi(a, b) ((a) &= ~(1 << (b)))    //clears bit B in variable A
 #define tbi(a, b) ((a) ^= 1 << (b))       //toggles bit B in variable A
 
-volatile uint8_t led = 0;
-volatile uint8_t count = 0;
-
-
-#define NUM_ADC_READ      2
+#define NUM_ADC_READ      3
 #define ADC_WHEEL_POS     0
 #define ADC_CURRENT_SENSE 1
+#define ADC_ROTATION      2
 
-volatile uint8_t adc_index     = 0;
-volatile uint8_t wheel_pos     = 0;
-volatile uint8_t current_sense = 0;
+#define MELEXUS_MAX         242
+#define MELEXUS_MIN         13
+
+#define ROTATION_OFFSET   .296
+
+volatile uint8_t  adc_index     = 0;
+volatile uint16_t wheel_dist    = 0;
+volatile uint8_t  current_sense = 0;
+
+volatile uint8_t  rotations     = 0;
+volatile uint8_t  wheel_pos     = 0;
+volatile uint8_t  last_wheel_pos= 0;
+volatile uint8_t  home_pos      = 0;
+
+volatile float    wheel_rotation     = 0;
 
 ISR (TIMER2_OVF_vect)
 {
@@ -43,12 +52,91 @@ ISR(ADC_vect)
     val = ADCH;
 
     if (adc_index == ADC_WHEEL_POS)
+    {
+        if (last_wheel_pos == 0)
+            last_wheel_pos = val;
         wheel_pos = val;
+
+        // did the wheel readings flip?
+        if (last_wheel_pos > wheel_pos && last_wheel_pos - wheel_pos > 100)
+            rotations++;
+        if (last_wheel_pos < wheel_pos && wheel_pos - last_wheel_pos > 100)
+            rotations--;
+
+        last_wheel_pos = wheel_pos; 
+        wheel_dist = (MELEXUS_MAX * rotations) + wheel_pos - home_pos;
+    }
     else
     if (adc_index == ADC_CURRENT_SENSE)
         current_sense = val;
+    else
+    if (adc_index == ADC_ROTATION)
+    {
+        float r = ((float)(val - MELEXUS_MIN) / (float)(MELEXUS_MAX - MELEXUS_MIN));
+        r -= ROTATION_OFFSET;
+        wheel_rotation = (r < 0.0) ? 1.0 + r : r;
+    }
 
     adc_index = (adc_index + 1) % NUM_ADC_READ;
+}
+
+uint16_t get_wheel_dist(void)
+{
+    uint16_t t;
+
+    cli();
+    t = wheel_dist;
+    sei();
+
+    return t;
+}
+
+int16_t get_rotation_deg(void)
+{
+    float r;
+
+    cli();
+    r = wheel_rotation;
+    sei();
+
+    return (int)(r * 360);
+}
+
+float get_rotation_rad(void)
+{
+    float r;
+
+    cli();
+    r = wheel_rotation;
+    sei();
+
+    return (int)(r * M_PI * 2.0);
+}
+
+void home(uint8_t motor)
+{
+    uint8_t cs;
+
+    // Bring the motor in until the current sensor kicks in
+    motor_speed(motor, 64, 0);
+    while(1)
+    {
+        cli();
+        cs = current_sense;
+        sei();
+        dprintf("cs: %d\n", cs);
+
+        if (cs < 88)
+            break;
+        _delay_ms(50); 
+    }
+    motor_speed(motor, 0, 0);
+
+    cli();
+    home_pos = wheel_pos;
+    rotations = 0;
+    sei();
+    dprintf("\nHome pos: %d\n", home_pos);
 }
 
 void timer_setup(void)
@@ -134,6 +222,27 @@ void motor_speed(uint8_t motor, uint8_t speed, uint8_t dir)
         OCR0B = speed;
 }
 
+void go_to(uint8_t motor, uint8_t speed, uint16_t dist)
+{
+    if (get_wheel_dist() < dist)
+    {
+        dprintf("go forward from %d to %d\n", get_wheel_dist(), dist);
+        motor_speed(motor, speed, 1);
+        while(get_wheel_dist() < dist)
+            _delay_ms(2);
+        //dprintf("wheel: %d dist: %d\n", get_wheel_dist(), dist);
+    }
+    else
+    {
+        dprintf("go backward from %d to %d\n", get_wheel_dist(), dist);
+        motor_speed(motor, speed, 0);
+        while(get_wheel_dist() > dist)
+            _delay_ms(2);
+    }
+    motor_speed(motor, 0, 1); 
+    dprintf("arrived at %d\n\n", get_wheel_dist());
+}
+
 // ramp the PWM output from from to to changing the value each ms
 void ramp(uint8_t motor, uint8_t dir, uint8_t from, uint8_t to, uint8_t ms_per_step)
 {
@@ -150,119 +259,49 @@ void ramp(uint8_t motor, uint8_t dir, uint8_t from, uint8_t to, uint8_t ms_per_s
     }
 }
 
-void home(uint8_t motor)
-{
-    uint8_t cs;
-
-    // Bring the motor in until the current sensor kicks in
-    motor_speed(motor, 64, 0);
-    while(1)
-    {
-        cli();
-        cs = current_sense;
-        sei();
-
-        if (cs < 95)
-            break;
-        _delay_ms(50); 
-    }
-    motor_speed(motor, 0, 0);
-
-    // Let chain out a little bit
-    motor_speed(motor, 32, 1);
-	_delay_ms(200); 
-    motor_speed(motor, 0, 1);
-}
-
-#define MAX_REV 242
-#define MIN_REV 13
-void wait_distance_out(int16_t distance)
-{
-    int16_t cur, last;
-    int16_t total = 0;
-    
-    last = adc_read(0);
-    while(total < distance)
-    {
-        cur = adc_read(0);
-        if (abs(cur - last) < 2)
-            continue;
-        if (cur < last)
-        {
-            total += MAX_REV - last;
-            last = MIN_REV;
-        }
-        total += cur - last;
-
-        last = cur;
-        _delay_ms(10);
-    }
-}
-
-void wait_distance_in(int16_t distance)
-{
-    int16_t cur, last;
-    int16_t total = 0;
-    
-    last = adc_read(0);
-    while(total < distance)
-    {
-        cur = adc_read(0);
-        if (cur > last)
-        {
-            total += last - MIN_REV;
-            last = MAX_REV;
-        }
-        total += last - cur;
-
-        last = cur;
-        _delay_ms(10);
-    }
-}
-
 int main(void)
 {
     uint8_t a, s;
+    uint16_t h;
 
     serial_init();
+
     adc_setup();
     pwm_setup();
     timer_setup();
 
+    // Motor driver setup: OUTA = black, OUTB = red
     // Pin 2 (PD2) is current sense
-    // Pin 3 is PWM out
+    // Pin 6 is PWM out
     // Pin 8 (PB0) is the motor direction.
     // Pin 13 (PB5) is the on board LED
+    // Pin A5 (PC5) is the Vcc for the rotatinal melexus
     DDRB |= (1<<PB0) | (1 << PB5);
+    DDRC |= (1<<PC5);
 
-    flash_led();
+    sbi(PORTD, 5);
+
+    //flash_led();
 
     sei();
     dprintf("poibot starting!\n");
     dprintf("Reset to home...");
     home(0);
 
-    while(1)
-    {
-        dprintf("pos: %d cs: %d\n", wheel_pos, current_sense);
-        _delay_ms(250);
-    }
+//    while(1)
+//    {
+//        dprintf("pos: %u last: %u rot: %u dist: %u\n", wheel_pos, last_wheel_pos, rotations, wheel_dist);
+//        _delay_ms(250);
+//    }
 
     s = 32;
+    h = wheel_dist;
 	while (1)
     {
         // Start letting out chain and ramp speed up
         //ramp(0, 1, 0, s, 2);
 
-
-        motor_speed(0, 32, 1);
-        wait_distance_out(150);
-        motor_speed(0, 0, 1);
-        _delay_ms(100);
-
-        motor_speed(0, 48, 0);
-        wait_distance_in(150);
-        motor_speed(0, 0, 0);
+        dprintf("%d\n", get_rotation_deg());
         _delay_ms(100);
     }
 
